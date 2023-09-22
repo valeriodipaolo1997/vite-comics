a148469fd4ae5cc856e30ba15c8d35fe2b2da224
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-09-22 21:43:45 +0200

    add components AppJumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppJumbotron from './components/AppJumbotron.vue';
+
 export default {
   data() {
     return {
     }
   },
   components: {
-    AppHeader
+    AppHeader,
+    AppJumbotron
   }
 }
 </script>
 
 <template>
   <AppHeader />
+  <AppJumbotron />
 </template>
 <style lang="scss"></style>
---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'AppJumbotron'
+}
+</script>
+
+<template>
+    <div id="jumbo" class="container">
+     
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/viariables' as *;
+#jumbo {
+    min-height: 400px; 
+    background-image: url('../assets/img/jumbotron.jpg');
+}
+</style>
