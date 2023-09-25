44c2cd3b1413ed24774b4f70c3b9d5ded504059c
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-09-25 15:46:30 +0200

    add components AppComics

---
+++ src/components/AppComics.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    name: 'AppComics',
+    props: {
+        thumb: String,
+        price: String,
+        series: String,
+        type: String
+    },
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="col pt-4">
+        <div class="card">
+            <img class="card-img-top" :src="thumb" alt="">
+            <div class="card-body ps-0">
+                <h6 class="card-title text-white text-uppercase"> {{ series }}</h6>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+    background-color: transparent;
+    img {
+        height: 185px;
+        object-fit: cover;
+        object-position: top;
+    }
+    h6 {
+        font-size: 0.9rem;
+    }
+}
+</style>
--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -1,22 +1,47 @@
 <script>
+import AppComics from './AppComics.vue';
+import comics from '../dc-comics';
+
 export default {
-    name: 'AppJumbotron'
+    name: 'AppJumbotron',
+    data() {
+        return {
+            comics: comics
+        }
+    },
+    components: {
+        AppComics,
+    }
+
 }
 </script>
 
 <template>
-    <div id="jumbo"> 
-     <div class="container pt-5">
-        <h3 class="text-white"> Content goes here </h3>
-     </div>
+    <div id="jumbo">
+        <img class="jumbo_img" src="../assets/img/jumbotron.jpg" alt="">
+
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 py-4">
+                <AppComics :thumb="comic.thumb" :series="comic.series" :type="comic.type" :price="comic.price"
+                    v-for="comic in comics" />
+            </div>
+        </div>
     </div>
 </template>
 
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/viariables' as *;
+
 #jumbo {
-   height: 150px; 
-    background-color: black;
+ 
+    background-color: $dc_darker;
+}
+
+.jumbo_img {
+    height: 400px;
+    width: 100%;
+    object-fit: cover;
+    object-position: top;
 }
 </style>
